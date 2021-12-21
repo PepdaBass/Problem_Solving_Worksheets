494ad37a5c5eedd85dc0abb2ab9577995c142179
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2021-12-21 13:07:30 -0500

    Updated compressor to omit numbers for single letters in uncompressed string.

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-# 1. Print string input backwards.
+# # 1. Print string input backwards.
 
-fun_string = input('Enter a word: ')
-backwards = True
-top_number = len(fun_string)
-index_number = -1
-backword = ''
-while backwards is True:
-    if top_number >= 1:
-        backword += fun_string[index_number]
-        top_number += -1
-        index_number += -1
-    else:
-        backwards = False
-        print(backword)
+# fun_string = input('Enter a word: ')
+# backwards = True
+# top_number = len(fun_string)
+# index_number = -1
+# backword = ''
+# while backwards is True:
+#     if top_number >= 1:
+#         backword += fun_string[index_number]
+#         top_number += -1
+#         index_number += -1
+#     else:
+#         backwards = False
+#         print(backword)
 
 
-# 2. Capitalize all words entered
+# # 2. Capitalize all words entered
 
-some_string = input('Enter some words: ')
-length_of_string = len(some_string)
+# some_string = input('Enter some words: ')
+# length_of_string = len(some_string)
 
-final_result = ''
+# final_result = ''
 
-for index in range(length_of_string):
-    if index == 0:
-        final_result += some_string[index].capitalize()
-    elif index >= 1 and some_string[index - 1] == ' ':
-        final_result += some_string[index].capitalize()
-    elif index >= 1 and some_string[index] != ' ':
-        final_result += some_string[index]
-    elif index >= 1 and some_string[index] == ' ':
-        final_result += some_string[index]
+# for index in range(length_of_string):
+#     if index == 0:
+#         final_result += some_string[index].capitalize()
+#     elif index >= 1 and some_string[index - 1] == ' ':
+#         final_result += some_string[index].capitalize()
+#     elif index >= 1 and some_string[index] != ' ':
+#         final_result += some_string[index]
+#     elif index >= 1 and some_string[index] == ' ':
+#         final_result += some_string[index]
        
-print(final_result)
+# print(final_result)
     
 
 # 3. Compressor
@@ -52,6 +52,10 @@ while compressed_yet is False:
         compressed_yet = True
     elif uncompressed_string[index] == compressed_string[-1]:
         compressed_string += uncompressed_string[index]
+    elif uncompressed_string[index] != compressed_string[-1] and len(compressed_string) == 1:
+        final_compressed += compressed_string[0] 
+        compressed_string = ''
+        compressed_string += uncompressed_string[index]
     elif uncompressed_string[index] != compressed_string[-1]:
         final_compressed += str(len(compressed_string)) + compressed_string[0] 
         compressed_string = ''
@@ -60,23 +64,23 @@ while compressed_yet is False:
 print(final_compressed)
         
 
-# 4. Palindrome Checker
+# # 4. Palindrome Checker
 
-palindrome = input('Enter a word a word you believe to be a palindrome: ')
-backwards = True
-top_number = len(palindrome)
-index_number = -1
-emordnilap = ''
-while backwards is True:
-    if top_number >= 1:
-        emordnilap += palindrome[index_number]
-        top_number += -1
-        index_number += -1
-    else:
-        backwards = False
-        print(emordnilap)
+# palindrome = input('Enter a word a word you believe to be a palindrome: ')
+# backwards = True
+# top_number = len(palindrome)
+# index_number = -1
+# emordnilap = ''
+# while backwards is True:
+#     if top_number >= 1:
+#         emordnilap += palindrome[index_number]
+#         top_number += -1
+#         index_number += -1
+#     else:
+#         backwards = False
+#         print(emordnilap)
 
-if palindrome == emordnilap:
-    print('Congratulations, this is a palindrome!')
-else:
-    print('Failure! You do not understand the concept!')
+# if palindrome == emordnilap:
+#     print('Congratulations, this is a palindrome!')
+# else:
+#     print('Failure! You do not understand the concept!')
